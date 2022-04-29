04eb45d4bea5bd208a54872860caedfb24d6621b
jawitcher <lion1n@mail.ru>
2022-04-29 19:26:54 +0500

    m

---
+++ src/leetcode/dp/MaximumSubarray.java
@@ -0,0 +1,19 @@
+package leetcode.dp;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+
+    }
+}
