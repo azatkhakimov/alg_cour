396b6b64d58a86c4173c007a097a7b770c1b50f4
jawitcher <lion1n@mail.ru>
2022-04-29 17:22:23 +0500

    m

---
+++ src/leetcode/recursive/FibonacciNumber.java
@@ -0,0 +1,28 @@
+package leetcode.recursive;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FibonacciNumber {
+    Map<Integer, Integer> cache = new HashMap<>();
+    public int fib(int N){
+        if(cache.containsKey(N)){
+            return cache.get(N);
+        }
+        int result;
+        if(N < 2){
+            result = N;
+        }else {
+            result = fib(N-1) + fib(N-2);
+        }
+
+        cache.put(N, result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        FibonacciNumber fibonacciNumber = new FibonacciNumber();
+//        System.out.println(fibonacciNumber.fib(2));
+        System.out.println(fibonacciNumber.fib(4));
+    }
+}
