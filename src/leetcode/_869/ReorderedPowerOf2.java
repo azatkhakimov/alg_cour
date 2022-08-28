ab758b77cc5e11b4f2953f3b899d666015cd120f
azatkhakimov <ayukhakimov@gmail.com>
2022-08-28 10:37:14 +0500

    leetcode 20220828

---
+++ src/leetcode/_869/ReorderedPowerOf2.java
@@ -0,0 +1,30 @@
+package leetcode._869;
+
+import java.util.Arrays;
+
+public class ReorderedPowerOf2 {
+    public boolean reorderedPowerOf2(int n) {
+        int[] a = count(n);
+        for (int i = 0; i < 31; i++) {
+            if(Arrays.equals(a, count(1 << i))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int[] count(int n) {
+        int[] ans = new int[10];
+        while (n>0){
+            ans[n%10]++;
+            n/=10;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        ReorderedPowerOf2 reorderedPowerOf2 = new ReorderedPowerOf2();
+        System.out.println(reorderedPowerOf2.reorderedPowerOf2(1));//Output: true
+        System.out.println(reorderedPowerOf2.reorderedPowerOf2(10));//Output: false
+    }
+}
