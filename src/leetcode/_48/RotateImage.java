76a515505fa0f6e70f6bb29c2a7f79750049765b
azatkhakimov <ayukhakimov@gmail.com>
2022-08-30 22:40:15 +0500

    leetcode 20220830

--- src/leetcode/_48/RotateImage.java
+++ src/leetcode/_48/RotateImage.java
@@ -1,19 +1,18 @@
-package leetcode.array;
-
-import java.util.Arrays;
+package leetcode._48;
 
 public class RotateImage {
-    public void rotate(int[][] matrix){
+    public void rotate(int[][] matrix) {
         transpose(matrix);
         reverse(matrix);
     }
+
     public void transpose(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-              int temp = matrix[j][i];
-              matrix[j][i] = matrix[i][j];
-              matrix[i][j] = temp;
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
             }
         }
     }
@@ -31,6 +30,17 @@ public class RotateImage {
 
     public static void main(String[] args) {
         RotateImage rotateImage = new RotateImage();
-        rotateImage.rotate(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        rotateImage.rotate(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        });//Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+        rotateImage.rotate(new int[][]{
+                {5, 1, 9, 11},
+                {2, 4, 8, 10},
+                {13, 3, 6, 7},
+                {15, 14, 12, 16}
+        });//Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]ma
     }
 }
